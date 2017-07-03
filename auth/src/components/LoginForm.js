2c42657af2dd462558a980cd7f7e91a13c1c2f61
papistan <jpapisan@gmail.com>
2017-07-02 22:19:50 -0700

    Update login form to include user auth calls

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,30 +1,71 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+// import files for authentication
 import { Button, Card, CardSection, Input } from './common';
 
 
 class LoginForm extends Component {
 
-	state = { text: '' };
+	state = { email: '', password: '', error: '' };
+
+	onButtonPress() {
+		const { email, password } = this.state;
+		
+		this.setState({ error: ''});
+
+		// code here that calls LOGIN request with (email, password) prob use axiom
+		// then 
+		.catch(() => {
+				// code for calling SIGN UP with (email, password)
+				.catch(() => {
+					this.setState({ error: 'Authentication Failed.' })
+					
+				});
+		});
+
+
+	}
 
 	render() {
 		return (
 			<Card>
 			<CardSection>
 				<Input 
-				value={this.state.text}
-				onChangeText={text => this.setState({ text })}
+				placeholder="user@gmail.com"
+				label="Email"
+				value={this.state.email}
+				onChangeText={email => this.setState({ email })}
 				/>
 			</CardSection>
 
-			<CardSection />
+			<CardSection>
+				<Input
+					secureTextEntry
+					placeholder="password"
+					value={this.state.password}
+					onChangeText={password => this.setState({ password })}
+				/>
+			</CardSection>
+
+			<Text>
+				{this.state.error}
+			</Text>
 
 			<CardSection>
-				<Button>
+				<Button onPress={this.onButtonPress.bind(this)}>
 					Log in
 				</Button>
 			</CardSection>
 			</Card>
 			)
+	}
+}
+
+const styles = {
+	errorTextStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
 	}
 }
 
